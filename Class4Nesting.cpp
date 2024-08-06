46975855604571917f01313cc1ed21ed8907ddb0
YashaswiSV <vermayashaswi1@gmail.com>
2024-08-06 23:32:11 +0530

    first commit

---
+++ Class4Nesting.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	                 //Nesting
+	                 
+//	int num; 
+//	cout<<"Enter a number: ";
+//	cin>>num;
+//	
+//	if(num%7==0){
+//		cout<<num<<" Divisible by 7 \n";
+//		   if(num%2==0){
+//		   	cout<<"It is even number ";
+//		   }else{
+//		   	cout<<"It is odd number ";
+//		   }
+//	}else{
+//		cout<<num<<" Not Divisible by 7 \n";
+//		 if(num%2==0){
+//		   	cout<<"It is even number ";
+//		   }else{
+//		   	cout<<"It is odd number ";
+//		   }
+//	}
+
+                        //Nesting
+                        
+        double tenth,twelth,degree;
+		int age ,exp;  
+		string name;
+		
+		cout<<"Enter your name: ";
+		cin>>name;
+		cout<<"Enther your age " ;
+		cin>>age>>exp;
+		cout<<"Enter 10th,12th,degree percentage: ";
+		cin>>tenth>>twelth>>degree;
+		cout<<"Enter your Experience: ";
+		cin>>exp;
+		
+		if(tenth>=60 && degree>=60)  
+		  
+		
+		
+	  cout              
+
+}
